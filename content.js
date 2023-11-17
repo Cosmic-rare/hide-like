4b23fce7a28c5f4ca78f3d9efd04c22634662321
tani <tani@tanis-MacBook-Pro.local>
2023-11-17 13:45:01 +0900

    完成！！！！

--- content.js
+++ content.js
@@ -5,19 +5,15 @@ chrome.storage.local.get('key', function (value) {
 });
 
 const myCallbackFunc = (mutations) => {
-  // すべてのaタグを取得
   var allLinks = document.querySelectorAll('a');
 
-  // 各リンクに対して処理を行う
   allLinks.forEach(function(link) {
-    // href属性が"/SkylineBNR334"の場合に"/tani_exe"に書き換える
     if (link.getAttribute('href') === '/' + user_id && getComputedStyle(link.parentElement).display === 'flex' && link.children.length === 4) {
 
-      // 親を15回まで遡り、最初に見つかったulタグに対してスタイルを適用する
       let parent = link.parentElement;
       for (let i = 0; i < 15 && parent; i++) {
         if (parent.tagName === 'UL') {
-          // ULタグの子要素が1個の場合はsolid 3px redにする
+
           if (parent.children.length === 1) {
             parent.style.border = 'solid 3px red';
             let currentParent = link.parentElement;
@@ -28,7 +24,6 @@ const myCallbackFunc = (mutations) => {
             }
             currentParent.remove()
           } else {
-            // ULタグの子要素が2個以上の場合、a要素を含むものは solid 3px red にする
             let currentParent = link.parentElement;
             let parentCount = 0;
             while (currentParent && parentCount < 5) {
@@ -37,11 +32,11 @@ const myCallbackFunc = (mutations) => {
             }
             currentParent.remove()
           }
+
           break;
         }
         parent = parent.parentElement;
       }
-
     }
   });
 };
